48c43ca42946fc13d268866d4396b98463f5a0c1
nuthakkinarendra <70637778+nuthakkinarendra@users.noreply.github.com>
2023-09-09 12:15:30 +0530

    Update database_students.sql

--- database_students.sql
+++ database_students.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE student;
 use student;
 
-CREATE TABLE student(
-    studentID int not null Auto_INCREMENT,
+CREATE TABLE students(
+    StudentID int not null Auto_INCREMENT,
     FirstName varchar(100) NOT NULL,
     Surname varchar(100) NOT NULL,
     PRIMARY KEY (StudentID)
 );
 
-INSERT INTO students(FirstName, Surname).
-VALUES("John", "Andersen"),
+INSERT INTO students(FirstName, Surname)
+VALUES("John", "Andersen"), ("narendra", "srikanth");
